07ddbb7bc68dd7f6d36a610b1d9bd9d3df86a592
stockcarrot <aurummc.yt@gmail.com>
2025-08-30 16:45:35 +0800

    Testing out bot commands

--- main.py
+++ main.py
@@ -31,4 +31,31 @@ async def on_message(message):
         await message.channel.send(f'Hello, {message.author.name}!')
     await bot.process_commands(message)
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def assign_role(ctx, role: discord.Role):
+    if role not in ctx.author.roles:
+        await ctx.author.add_roles(role)
+        await ctx.send(f'Role {role.name} has been assigned to you.')
+    else:
+        await ctx.send(f'You already have the role {role.name}.')
+
+@bot.command()
+async def remove_role(ctx, role: discord.Role):
+    if role in ctx.author.roles:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f'Role {role.name} has been removed from you.')
+    else:
+        await ctx.send(f'You never had the role {role.name}.')
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
