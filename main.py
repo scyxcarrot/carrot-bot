a23c327c888e5c9ce386d0bed020e8f7681e00b7
stockcarrot <aurummc.yt@gmail.com>
2025-08-30 16:19:25 +0800

    Testing out bot events

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-token = os.getenv('DISCORD_BOT_TOKEN')
+token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
@@ -13,3 +13,22 @@ intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print(f'Bot ID: {bot.user.id}')
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f'Welcome to the server, {member.name}!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if "hello" in message.content.lower():
+        await message.channel.send(f'Hello, {message.author.name}!')
+    await bot.process_commands(message)
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
